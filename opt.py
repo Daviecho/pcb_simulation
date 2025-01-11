e9ad750066a17945d7738420bcf9bcf576bcfbac
Florian <florian_stamer@yahoo.de>
2025-01-11 16:07:13 +0100

    .

--- opt.py
+++ opt.py
@@ -2,6 +2,12 @@
 import optuna
 import os
 from main import main_function
+import os
+from dotenv import load_dotenv  # Import dotenv to load environment variables
+
+load_dotenv()
+
+OPT_STEPS = int(os.getenv("OPT_STEPS", 4))
 
 def objective(trial):
     # Suggest piecewise parameters for test bonus as an example
@@ -24,7 +30,7 @@ def objective(trial):
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=4)
+    study.optimize(objective, n_trials=OPT_STEPS)
 
     print("Best parameters:", study.best_params)
     print("Best value:", study.best_value)
